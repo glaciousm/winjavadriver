1e8905ba58f96a8c4794917dbb8e020d480d15fd
Menelaos Mamouzellos <mmamouzellos@gmail.com>
2026-03-09 13:00:22 +0200

    chore(client): bump VERSION to 1.0.2

--- client-java/src/main/java/io/github/winjavadriver/WinJavaDriverService.java
+++ client-java/src/main/java/io/github/winjavadriver/WinJavaDriverService.java
@@ -35,7 +35,7 @@ public class WinJavaDriverService extends DriverService {
     public static final int DEFAULT_PORT = 9515;
     public static final String GITHUB_RELEASES_URL =
             "https://github.com/glaciousm/winjavadriver/releases";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     public WinJavaDriverService(
             File executable,
